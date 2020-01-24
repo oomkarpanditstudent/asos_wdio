4062d5377744d385108a7cb56994204952940e96
Oomkar Pandit <lolasofteware@gmail.com>
2020-01-24 12:18:18 +0000

    Customer Journey going to Sign in Page, update the test

--- tests/homePage.js
+++ tests/homePage.js
@@ -5,20 +5,32 @@ describe("Test Suite - Customer Journeys for asos.com", ()=>{
         assert.equal(title,"ASOS | Online Shopping for the Latest Clothes & Fashion");
     })*/
     it("Test Case 2 - Browse to Women/Dresses", ()=>{
+        
+        // Variables
+        const subMenuDresses=$('[href*="women/dresses"][data-testid="text-link"]');
+        const clothingHover=$('span=Clothing');
+        const bagIcon=$('[data-testid="bagIcon"]');
+        const bagCheckout=$('span=Checkout')
+        //Open site and go to Women Category - From Home Page
         browser.url('/');
         browser.$('[data-testid="women-floor"]').click();
-        const subMenuDresses=$('[href*="women/dresses"][data-testid="text-link"]');
-        const clothingHover=$('#chrome-sticky-header div:nth-of-type(2) > div:nth-of-type(1) .otVu6ZN:nth-of-type(2) .f3gYf7g span');
-       // browser.execute(() => { $('._1qgAFfV .f3gYf7g span').mouseover(); });
-       //clothingHover.waitForVisibke
-       clothingHover.moveTo() ;
-       subMenuDresses.waitForClickable(7000);
-    /*
-        subMenuDresses.waitForExist( 7000 );
-       subMenuDresses.waitForDisplayed(7000);
-      */
-      subMenuDresses.click();
-      //browser.$('[href*="women/dresses"][data-testid="text-link"]').click();
+        // Hover to Clothing and go to Dresses - Category Page
+        clothingHover.moveTo() ;
+        subMenuDresses.waitForClickable(7000);
+        subMenuDresses.click();
+        //Pick items from the PLP - PLP Page
+        browser.$('section > article:nth-of-type(3)').click(); //select 3rd product from list
+        //Interactions - Journey Flow - PDP Page 
+        browser.$('select#main-size-select-0').selectByIndex(2); //select 2nd indexed item
+        browser.$('.aside-action a').click(); //add to bag/
+        bagIcon.moveTo(); // hovering to enable mini bag options
+        bagCheckout.waitForClickable(7000); // ensuring the checkout is available to click on bag page
+        bagCheckout.click(); //clicked checkout of mini bag
+        assert.equal(browser.getTitle(),'ASOS | Sign in'); // asserted that we are on login page
+        
+
+   
+
 
     })
 
