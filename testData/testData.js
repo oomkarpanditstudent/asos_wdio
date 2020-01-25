a4bde59a62e827dae4cd385c9209cdfb84d628af
Oomkar Pandit <lolasofteware@gmail.com>
2020-01-25 03:22:12 +0000

    annonymised test date

--- testData/testData.js
+++ testData/testData.js
@@ -1,13 +1,13 @@
  const testDataProd ={
-    firstName: "Oomkar",
-    lastName: "Pandit",
-    email:"lolasoftware@gmail.com",
+    firstName: "Satnam",
+    lastName: "Waheguru",
+    email:"jaimatadi@gmail.com",
     address:"24 Sipson Road",
-    mobile:"0794444408",
+    mobile:"0794444409",
     cardNumberVisa:"4444333322221111",
     cardExpiryMonth:'10',
     cardExpiryYear:'2020',
-    cardName:"Oomkar Pandit QA",
+    cardName:"Visa Card Owner",
     cvv:"123",
     size:"2", 
 
