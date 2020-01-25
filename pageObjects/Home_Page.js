2669dedb53adff52c43bd0cbbcf658a267fced92
Oomkar Pandit <lolasofteware@gmail.com>
2020-01-25 01:36:44 +0000

    Page Objects in progress

--- pageObjects/Home_Page.js
+++
@@ -1,9 +0,0 @@
-class Home_Page {
-
-    get womenTopNav() { return $('[data-testid="women-floor"]');}
-
-    openWomenTopNav(){
-        return this.womenTopNav.click();
-    }  
-}
-module.exports = new Home_Page()
--- pageObjects/Plp_Page.js
+++
@@ -1,9 +0,0 @@
-class Plp_Page {
-     
-    get thirdPlpItem() { return $('section > article:nth-of-type(3)');}
-    
-    pickPlpItem(){
-        this.thirdPlpItem.click;
-        }
-}
-module.exports = new Plp_Page()
